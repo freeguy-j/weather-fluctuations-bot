c083d923aa0ed3d6a803306af48c1189ceb16ff5
InSeongJang <inseongj2@gmail.com>
2021-08-23 23:12:08 +0900

    first commit

---
+++ src/main/java/com/inseong/weatherfluctuations/WeatherFluctuationsApplication.java
@@ -0,0 +1,13 @@
+package com.inseong.weatherfluctuations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherFluctuationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherFluctuationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inseong/weatherfluctuations/WeatherFluctuationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inseong.weatherfluctuations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherFluctuationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
